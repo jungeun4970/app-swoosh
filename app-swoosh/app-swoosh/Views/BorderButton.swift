b0ffd3c3d2560543c7ce06a26c55709cfd0f72bb
JoonChoi <cjg4970@naver.com>
2021-03-11 17:25:28 +0900

    first edit

---
+++ app-swoosh/app-swoosh/Views/BorderButton.swift
@@ -0,0 +1,18 @@
+//
+//  BorderButton.swift
+//  app-swoosh
+//
+//  Created by Choi Joon on 2021/03/11.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.borderWidth = 3.0
+        layer.borderColor = UIColor.white.cgColor 
+    }
+
+}
