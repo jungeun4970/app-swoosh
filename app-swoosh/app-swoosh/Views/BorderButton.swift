1d5235f43dce35828d63747b93b5fb27bd797ec3
JoonChoi <cjg4970@naver.com>
2021-03-11 17:29:53 +0900

    button size

--- app-swoosh/app-swoosh/Views/BorderButton.swift
+++ app-swoosh/app-swoosh/Views/BorderButton.swift
@@ -12,7 +12,7 @@ class BorderButton: UIButton {
     override func awakeFromNib() {
         super.awakeFromNib()
         layer.borderWidth = 3.0
-        layer.borderColor = UIColor.white.cgColor 
+        layer.borderColor =  UIColor.white.cgColor 
     }
 
 }
