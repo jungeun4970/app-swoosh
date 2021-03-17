a63a04e9f2ef9491cbf66be63d2b91a51c23477d
JoonChoi <cjg4970@naver.com>
2021-03-17 16:13:26 +0900

    MVC Pattern

---
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -0,0 +1,29 @@
+//
+//  LeagueVC.swift
+//  app-swoosh
+//
+//  Created by Choi Joon on 2021/03/17.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- app-swoosh/app-swoosh/Controller/ViewController.swift
+++ app-swoosh/app-swoosh/Controller/ViewController.swift
@@ -7,11 +7,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    @IBAction func unwindFromSkiilVC(unwindSegue : UIStoryboardSegue){
+        
     }
 
 
